91721b8bd023179ad8196d78c0c4430f1d3f6743
edgar <edgar@t2s.com.br>
2024-04-09 11:00:41 -0300

    add controle de acesso

---
+++ EmitEasy.API/Controllers/AcessoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmitEasy.API.Controllers
+{
+    [Route("api/acesso")]
+    [ApiController]
+    public class AcessoController: ControllerBase
+    {
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get() 
+        {
+            return Ok("Acesso Permitido!");
+        }
+    }
+}
